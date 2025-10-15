2be95497a1e2e4b3367d64c7befc3530f593b636
robbotics-code <r.morrison2219@gmail.com>
2025-10-15 15:57:04 -0500

    Redeploy

--- src/app/slt/success/page.tsx
+++ src/app/slt/success/page.tsx
@@ -1,17 +1,52 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-
+'use client'
+import Link from "next/link";
 import "@/app/styles/slt.css";
-import { Suspense } from 'react';
+import { Suspense, useEffect } from "react";
+import { useSearchParams } from 'next/navigation'
 import { PageWrapper } from "@/app/components/PageWrapper";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { useCart } from '@/app/slt/components/CartProvider';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 export default function Success(){
+    const { state } = useCart();
+    const { dispatch } = useCart();
+    const searchParams = useSearchParams();
+    const email = searchParams.get('email');
+    useEffect(() => {
+            async function sendReceipt(){
+                const receiptRes = await fetch('../../api/email', {
+                    headers: {'Content-Type': 'application/json'},
+                    method: 'POST',
+                    body: JSON.stringify({
+                        firstName: '',
+                        email: email,
+                        success: true,
+                        items: state.items
+                    })
+                })
+
+                if(!receiptRes.ok)
+                    throw new Error('Receipt Failed to Send.');
+                else 
+                    state.items.map(item => dispatch({type: 'REMOVE_ITEM', payload: item.id }))
+            }
+            sendReceipt()
+        
+    }, [])
     return(
         <PageWrapper>
             <Suspense fallback={<div><FontAwesomeIcon style={{fontSize: '75px'}} icon={"fa-solid fa-spinner" as IconProp}  spinPulse size="2xl" /></div>}>
-                <Success/>
+                <div style={{alignItems: 'center'}} className="slt-secondary-bg slt-success-container">
+                    <img src={'../img/success.gif'}/>
+                    <h1>Your Purchase was successful!!!</h1>
+                    <div>
+                        <Link href='/slt'>Home</Link>
+                        <Link href='/slt/menu'>Menu</Link>
+                    </div>
+                </div>
             </Suspense>
         </PageWrapper>
     )
